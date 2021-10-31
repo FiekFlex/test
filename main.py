beed0905ce936fdb7a04c0fa9653dd6f24f32e3b
FiekFlex <fiekflex@gmail.com>
2021-10-31 11:40:39 +0300

    Холст для игры

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from tkinter import *
+import random
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tk = Tk()
+tk.title("igra")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+while 1:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
