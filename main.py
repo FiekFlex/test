04a4743da9455209347be1ed653112de67b51911
FiekFlex <fiekflex@gmail.com>
2021-11-01 14:33:21 +0300

    Черепашки 1.0

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ while 1:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+
+import turtle
+avery = turtle.Pen()
+kate= turtle.Pen()
+avery.forward(50)
+avery.forward(90)
+avery.forward(20)
+kate.left(90)
+kate.forward(100)
+
+jacob= turtle.Pen()
+jacob.left(180)
+jacob.forward(80)
