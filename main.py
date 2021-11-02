47d3fe001c0d3f97ff332eae97ebb1827be47266
FiekFlex <fiekflex@gmail.com>
2021-11-02 14:24:40 +0300

    прыг-скок от стенок

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ from tkinter import *
 import random
 import time
 
-from tkinter import *
-import random
-import time
-
 
 class Ball:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
 
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
+
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
 
 
 tk = Tk()
-tk.title("Игра")
+tk.title("Прыг-скок")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
 
@@ -29,6 +41,7 @@ tk.update()
 ball = Ball(canvas, "red")
 
 while 1:
+    ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
